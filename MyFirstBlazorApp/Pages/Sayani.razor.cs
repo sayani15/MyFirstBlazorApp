47575bbbe8557894d13c7ceec6b232123a9406f4
Sayani Pathak <sayanipathak01@gmail.com>
2021-12-30 18:26:46 +0000

    Add project files.

---
+++ MyFirstBlazorApp/Pages/Sayani.razor.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyFirstBlazorApp.Pages
+{
+    public partial class Sayani
+    {
+        public bool IsHappyNumber(int number, int interations = 1000000) //number = 7
+        {
+            for (int i = 0; i < interations; i++)
+            {
+                number = GetSumOfDigitsSquared(number);
+
+                if (number == 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Calculates the sum of squares of each digit in the passed number.
+        /// </summary>
+        /// <param name="number"> The number to find the sum of the squared digits.</param>
+        public int GetSumOfDigitsSquared(int number)
+        {
+
+            if (number <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Number must be greater than 0.");
+            }
+
+            var numberAsAString = number.ToString();
+
+            var sumOfSquares = 0;
+
+            foreach (var digit in numberAsAString)
+            {
+                var d = char.GetNumericValue(digit);
+                var digitSquared = Math.Pow(d, 2);
+                sumOfSquares += (int)digitSquared;
+            }
+            return sumOfSquares;
+        }
+
+        public int YrsInARange(List<int> rangeOfYrs)
+        {
+            var repeatedDigitCount = 0;
+
+            foreach (var year in rangeOfYrs)
+            {
+                if (year <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Year must be greater than 0.");
+                }
+                else
+                {
+                    var digitsDict = new Dictionary<int, string>();
+
+                    var yearAsAString = year.ToString();
+
+                    bool duplicateAdded = false;
+
+                    foreach (var digit in year.ToString())
+                    {
+                        var digitAsNum = char.GetNumericValue(digit);
+
+                        try
+                        {
+                            digitsDict.Add((int)digitAsNum, digitAsNum.ToString());
+                        }
+                        catch (Exception)
+                        {
+                            if (!duplicateAdded)
+                            {
+                                repeatedDigitCount += 1;
+                                duplicateAdded = true;
+                            }
+                        }
+                    }
+                }
+            }
+            return repeatedDigitCount;
+        }
+
+        public double Product(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public string ReverseString(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            string reversed = String.Empty;
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                reversed += charArray[i];
+            }
+            return reversed;
+        }
+
+        /// <summary>
+        /// Determines whether a word is a palindrome.
+        /// </summary>
+        /// <param name="text">The word to test.</param>
+        public bool IsPalindrome(string text)
+        {
+            text = text.ToUpper();
+
+            char[] charArray = text.ToCharArray();
+            string reversed = "";
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                reversed += charArray[i];
+            }
+
+            if (reversed == text)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MyFirstBlazorAppTests/SayaniTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace MyFirstBlazorApp.Pages.Tests
+{
+    [TestClass()]
+    public class SayaniTests
+    {
+        [TestMethod()]
+        public void YrsInARangeTest()
+        {
+            var sayani = new Sayani();
+            var years = new List<int>() { 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 };
+            var yearsNegative = new List<int>() { -2012 };
+            var result = sayani.YrsInARange(years);
+
+            Assert.AreEqual(2, result);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => sayani.YrsInARange(yearsNegative));
+        }
+
+        [TestMethod()]
+        public void GetSumOfDigitsSquaredTest()
+        {
+            var sayani = new Sayani();
+            var result = sayani.GetSumOfDigitsSquared(2);
+            var result2 = sayani.GetSumOfDigitsSquared(5);
+            var result3 = sayani.GetSumOfDigitsSquared(13);
+            var result4 = sayani.GetSumOfDigitsSquared(15);
+            var result5 = sayani.GetSumOfDigitsSquared(156);
+            var result6 = sayani.GetSumOfDigitsSquared(14567);
+
+            Assert.AreEqual(4, result);
+            Assert.AreEqual(25, result2);
+            Assert.AreEqual(10, result3);
+            Assert.AreEqual(26, result4);
+            Assert.AreEqual(62, result5);
+            Assert.AreEqual(127, result6);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => sayani.GetSumOfDigitsSquared(-6));
+
+        }
+
+        [TestMethod()]
+        public void ProductTest()  
+        {
+            var sayani = new Sayani();
+            var result1 = sayani.Product(15,4);
+            var result2 = sayani.Product(9, 4);
+            var result3 = sayani.Product(150, 4);
+
+            Assert.AreEqual(60, result1);
+            Assert.AreEqual(36, result2);
+            Assert.AreEqual(600, result3);
+
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+            var sayani = new Sayani();
+            var result1 = sayani.IsPalindrome("hello");
+            var result2 = sayani.IsPalindrome("racecar");
+            var result3 = sayani.IsPalindrome("ava");
+            var result4 = sayani.IsPalindrome("bye");
+            var result5 = sayani.IsPalindrome("Racecar");
+
+            Assert.IsFalse(result1);
+            Assert.IsTrue(result2);
+            Assert.IsTrue(result3);
+            Assert.IsFalse(result4);
+            Assert.IsTrue(result5);
+
+        }
+
+        [TestMethod()]
+        public void IsHappyNumber()
+        {
+            var sayani = new Sayani();
+            var result1 = sayani.IsHappyNumber(7);
+            var result2 = sayani.IsHappyNumber(56);
+            var result3 = sayani.IsHappyNumber(109);
+            var result4 = sayani.IsHappyNumber(5);
+            var result5 = sayani.IsHappyNumber(13);
+
+            // 7, 13, 19, 23, 31, 79, 97, 103, 109, 139
+
+            Assert.IsTrue(result1);
+            Assert.IsFalse(result2);
+            Assert.IsTrue(result3);
+            Assert.IsFalse(result4);
+            Assert.IsTrue(result5);
+
+        }
+
+    }
+}
