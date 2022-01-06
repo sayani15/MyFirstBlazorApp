3773ccc9c9cf23e2ff95aabdbdea09b106acd3b5
Sayani Pathak <sayanipathak01@gmail.com>
2022-01-06 19:33:38 +0000

    Added Element and form.

---
+++ MyFirstBlazorApp/DataStructures/ListElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstBlazorApp.DataStructures
+{
+    public class ListElement
+    {
+        public string name = "";
+        public DateTime dueDate = new DateTime();
+        public string whoseResponsibility = "";
+        public string description = "";
+        public DateTime whenItWasAdded = new DateTime();
+    }
+}
---
+++ MyFirstBlazorApp/Pages/ToDoList.razor.cs
@@ -0,0 +1,27 @@
+﻿using MyFirstBlazorApp.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstBlazorApp.Pages
+{
+    public partial class ToDoList
+    {
+        public ListElement ListElement { get; set; } = new ListElement();
+
+        public bool CanDisplayElement { get; set; } = false;
+
+        public ToDoList()
+        {
+            ListElement.name = "hi";
+        }
+
+        public void DisplayElement()
+        {
+            CanDisplayElement = true;
+            
+        }
+
+    }
+}
